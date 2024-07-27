5986fbc84dd5af16f8d23a63e2f630735e29c27a
Jose Benitez <pepe@Joses-MacBook-Pro.local>
2024-07-27 01:24:19 +0100

    I think I might be having problems with data types in the raw data

--- scripts/ppml.py
+++ scripts/ppml.py
@@ -1,7 +1,30 @@
+import gme as gm
 import pandas as pd
-import numpy as np
-import gegravity as geg
 
 # Load data
 file_path = './data/raw_data/cepii/Gravity_csv_V202211/Gravity_V202211.csv'
-df_gravity = pd.read_csv(file_path, low_memory=False)
+data = pd.read_csv(file_path, low_memory=False)
+
+# Subset of the data for testing
+data_subset = data.sample(n=1000, random_state=1)
+
+# Create EstimationData object
+est_data = gm.EstimationData(data_frame=data_subset,
+                                   imp_var_name='country_id_d',
+                                   exp_var_name='country_id_o',
+                                   trade_var_name='tradeflow_comtrade_o', # COMTRADE trade flows from origin to destination as declared by exporter
+                                   year_var_name='year')
+
+# print(est_data)
+
+# Define the gravity model
+model = gm.EstimationModel(
+    estimation_data=est_data,
+    lhs_var='tradeflow_comtrade_o',  # Dependent variable
+    rhs_var=['gdp_o', 'gdp_d', 'dist', 'comlang_off', 'contig', 'col_dep_ever', 'pop_o', 'pop_d'],  # Independent variables
+    fixed_effects=[['country_id_d','year'],['country_id_o','year']]  # Fixed effects
+)
+
+results = model.estimate()
+print(results.summary())
+# print(model.ppml_diagnostics)
